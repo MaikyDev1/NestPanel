0582335bd9ae6096321054d1c5796331d22445d1
maikydev <maikydev@icloud.com>
2025-12-12 09:23:52 +0200

    Updated some ui elements

--- src/app/components/BaseUI.jsx
+++ src/app/components/BaseUI.jsx
@@ -12,15 +12,17 @@ export function StatelessSceneBoxUI({title, description, icon, sceneId}) {
     setPopup(true);
   }
   return (
-    <div className="cursor-pointer select-none hover:bg-neutral-700/30 flex flex-col justify-center items-center h-20 w-20 rounded-2xl">
+    <div className="group cursor-pointer select-none hover:bg-amber-400/30 bg-neutral-400/10 text-neutral-800 flex flex-col justify-center h-20 w-full p-4 rounded-2xl">
       <PopUp icon="line-md:confirm-circle" setPopup={setPopup} popup={popup} title={`${title} was runned`}/>
-      <div id={sceneId} className="flex rounded-2xl items-center justify-center"
-           onClick={turn}>
-        <div className="flex p-1 items-center justify-center flex-col">
-          <Icon className="text-4xl" icon={icon}/>
+      <div id={sceneId} className="flex rounded-2xl items-center" onClick={turn}>
+        <div className=" flex p-1 items-center justify-center flex-col">
+          <Icon className="text-4xl grayscale group-hover:grayscale-0" icon={icon}/>
+        </div>
+        <div className="flex flex-col">
+          <p className="font-semibold text-sm">{title}</p>
+          <p className="font-thin text-xs">{description}</p>
         </div>
       </div>
-      <p className="font-semibold mt-1 text-sm">{title}</p>
     </div>
   )
 }
@@ -34,15 +36,34 @@ export function StateSceneBoxUI({currentState, title, description, icon, sceneId
     setPopup(true);
   }
   return (
-    <div className={"cursor-pointer select-none hover:bg-neutral-700/30 flex flex-col justify-center items-center h-20 w-20 rounded-2xl " + (state ? "bg-amber-400/60" : "")}>
+    <div className={`group cursor-pointer select-none hover:bg-amber-400/30 ${state ? "bg-amber-400/40" : "bg-neutral-400/10"} text-neutral-800 flex flex-col justify-center h-20 w-full p-4 rounded-2xl`}>
       <PopUp icon="line-md:confirm-circle" setPopup={setPopup} popup={popup} title={`${title} was runned`}/>
-      <div id={sceneId} className={"flex rounded-2xl items-center justify-center "}
-           onClick={turn}>
-        <div className="flex p-1 items-center justify-center flex-col">
-          <Icon className="text-4xl" icon={icon}/>
+      <div id={sceneId} className="flex rounded-2xl items-center" onClick={turn}>
+        <div className=" flex p-1 items-center justify-center flex-col">
+          <Icon className={`text-4xl ${state ? "grayscale-0" : "grayscale"} group-hover:grayscale-0`} icon={icon}/>
+        </div>
+        <div className="flex flex-col">
+          <p className="font-semibold text-sm">{title}</p>
+          <p className="font-thin text-xs">{description}</p>
         </div>
       </div>
-      <p className="font-semibold text-sm">{title}</p>
+    </div>
+  )
+}
+
+export function NestBoxUI({currentState, meta, description, icon, color, devices_count, nestId}) {
+  const [state, setState] = useState(currentState);
+  return (
+    <div className={`group cursor-pointer select-none hover:bg-amber-400/30text-neutral-800 flex justify-center aspect-square w-full p-4 rounded-2xl`} style={{background: `${color}20`}}>
+      <div id={nestId} className="flex rounded-2xl gap-5 flex-col justify-center items-center">
+        <div className="flex items-center justify-center flex-col rounded-full p-3" style={{background: `${color}50`}}>
+          <Icon className={`text-4xl`} style={{color: color}} icon={icon}/>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <p className="font-semibold text-sm">{meta.title}</p>
+          <p className="font-thin text-xs">{devices_count === undefined ? "" : devices_count + " devices"}</p>
+        </div>
+      </div>
     </div>
   )
 }
@@ -79,7 +100,7 @@ export function ErrorBox({children}) {
 export function PopUp({icon, title, description, popup, setPopup}) {
   if (!popup) return null;
   return (
-    <main className="absolute top-0 left-0 h-screen w-screen flex justify-center items-center bg-black/20 backdrop-blur">
+    <main className="z-10 absolute top-0 left-0 h-screen w-screen flex justify-center items-center bg-black/20 backdrop-blur">
       <div className={"lg:w-1/5 md:w-1/3 w-full mx-4 rounded-2xl flex flex-col justify-between items-center py-10 bg-white shadow-2xl"}>
         <div className="flex flex-col items-center justify-center">
           <Icon icon={icon} className="text-7xl text-green-400"/>
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 import useSWR from "swr";
 import {
   ErrorBox,
-  LoadingContent,
+  LoadingContent, NestBoxUI,
   SceneBoxUI,
   StatelessSceneBoxUI,
   StateSceneBoxUI,
@@ -20,15 +20,15 @@ export default function Home() {
         <div className="bg-white pt-2 rounded-t-2xl flex flex-col flex-grow">
           <div className="flex flex-col  w-full p-2">
             <h1 className="text-neutral-900 font-thin ml-2">Scenes</h1>
-            <div className="grid md:grid-cols-3 grid-cols-2 gap-3">
+            <div className="grid grid-cols-2 gap-3">
               <Scenes/>
             </div>
           </div>
           {/* Devices */}
           <div className="flex flex-col justify-center w-full p-2">
             <h1 className="text-neutral-900 font-thin ml-2">Nests</h1>
-            <div className="grid md:grid-cols-2 grid-cols-2 gap-3">
-              <Devices/>
+            <div className="grid grid-cols-2 gap-3 mt-3">
+              <Nests/>
             </div>
           </div>
         </div>
@@ -40,16 +40,41 @@ const fetcher = url => fetch(url).then(r => r.json())
 
 function Header() {
   return (
-    <div className="p-3">
-      <div className="flex items-center p-3 gap-4">
-        <div className="flex items-center p-3 gap-4">
+    <div className="p-6">
+      <div className="flex flex-col justify-center gap-4">
+        <div className="flex items-center gap-4">
           <Icon icon="duo-icons:dashboard" className="text-4xl text-white/50 ring ring-white/50 rounded-lg p-1"/>
           <div className="flex flex-col text-white">
             <p><span className="font-bold">Welcome</span> to NestHome</p>
             <p className="text-sm font-thin">Made by Maiky</p>
           </div>
         </div>
-
+      {/* Examples */}
+        <nav className="flex justify-between px-4">
+          {/* Temperature */}
+          <div className="flex items-center flex-col">
+            <div className="flex items-center text-2xl text-white">
+              <p className="font-bold font-mono">22</p>
+              <Icon icon="fluent:temperature-degree-celsius-16-filled" className="text-3xl"/>
+            </div>
+            <p className="text-white font-normal text-sm">Bedroom temp</p>
+          </div>
+          {/* Some state */}
+          <div className="flex items-center flex-col">
+            <div className="flex items-center text-2xl text-white">
+              <p className="font-bold font-mono">-4</p>
+              <Icon icon="fluent:temperature-degree-celsius-16-filled" className="text-3xl"/>
+            </div>
+            <p className="text-white font-normal text-sm">Outside temp</p>
+          </div>
+          {/* Current consumtion */}
+          <div className="flex items-center flex-col">
+            <div className="flex items-center text-2xl text-white">
+              <p className="font-bold font-mono">0.2KW</p>
+            </div>
+            <p className="text-white font-normal text-sm">Home consumtion</p>
+          </div>
+        </nav>
       </div>
     </div>
   )
@@ -63,22 +88,22 @@ function Scenes() {
   let html = [];
   data.forEach(part => {
     html.push(part.scene_type === "STATELESS" ?
-      <StatelessSceneBoxUI key={part.id} title={part.name} icon={part.icon} sceneId={part.id}/> :
-      <StateSceneBoxUI currentState={part.current_state} key={part.id} title={part.name} icon={part.icon} sceneId={part.id}/>
+      <StatelessSceneBoxUI key={part.id} title={part.name} description={part.description} icon={part.icon} sceneId={part.id}/> :
+      <StateSceneBoxUI currentState={part.current_state} description={part.description} key={part.id} title={part.name} icon={part.icon} sceneId={part.id}/>
     )
   })
   return html;
 }
 
-function Devices() {
-  const {data, error, isLoading} = useSWR("/api/v1/devices/get", fetcher)
+function Nests() {
+  const {data, error, isLoading} = useSWR("/api/v1/nests/get", fetcher)
   if (error) return <ErrorBox>{JSON.stringify(error)}</ErrorBox>;
   if (isLoading) return <LoadingContent/>;
   if (data === undefined) return <ErrorBox>Data is undifiend</ErrorBox>;
   let html = [];
   data.forEach(part => {
     html.push(
-      <TestDeviceObject key={part.id} part={part}/>
+      <NestBoxUI key={part.id} currentState={false} meta={part.meta} icon={part.icon} color={part.color} devices_count={part.devices_count} nestId={part.id}/>
     )
   })
   return html;
